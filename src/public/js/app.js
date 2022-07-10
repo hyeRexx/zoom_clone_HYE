33b6f74a9e67310c61722e483236a7f1f8fbfd04
hyeRexx <caco3.rinrin@gmail.com>
2022-07-10 20:48:36 +0900

    middle

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,46 +1,97 @@
-const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("#message");
-const nickForm = document.querySelector("#nickname");
-const socket = new WebSocket(`ws://${window.location.host}`)
+const socket = io();
 
-function makeMessage(type, payload) {
-    const msg = {nickname, type, payload};
-    return JSON.stringify(msg);
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const camSelect = document.getElementById("cameras");
+const welcome = document.getElementById("welcome");
+const call = document.getElementById("call");
+const welcomeForm = welcome.querySelector("form");
+
+call.hidden = true;
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getCamera() {
+    try{
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => device.kind === 'videoinput');
+        const currCam = myStream.getVideoTracks()[0];
+        cameras.forEach((camera) => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            if (currCam == camera.label) {
+                option.selected = true;
+            }
+            camSelect.appendChild(option);
+        })
+    } catch(e) {
+        console.log(e);
+    }
 }
 
-socket.addEventListener("open", () => {
-    console.log("Connected to Sever 😁")
-});
-
-socket.addEventListener("message", (msg) => {
-    const message = JSON.parse(msg.data);
-    switch (message.type) {
-        case "new_message" :
-            const li = document.createElement("li");
-            li.innerText = `${message.nickname} : ${message.payload}`
-            messageList.append(li);
-            break;
+async function getMedia(deviceId) {
+    const initialConstrains = {
+        audio : true,
+        video : {facingMode : "user"},
     }
-});
+    const cameraConstrains = {
+        audio : true,
+        video : {deviceId : {exact : deviceId}},
+    }
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId ? cameraConstrains : initialConstrains
+        );                     
+        myFace.srcObject = myStream;
+        if (!deviceId) {
+            await getCamera();
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
 
-// 서버 죽었을 때
-socket.addEventListener("close", () => {
-    console.log("Connected from Server X");
-});
+function handleMuteClick() {
+    myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled));
+    if(!muted) {
+        muteBtn.innerText = "Unmute";
+        muted = true;
+    } else {
+        muteBtn.innerText = "Mute";
+        muted = false;
+    }
+}
 
-messageForm.addEventListener("submit", (event) => {
+function handleCameraClick() {
+    myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled));
+    if (cameraOff) {
+        cameraBtn.innerText = "Camera Off";
+        cameraOff = false;
+    } else {
+        cameraBtn.innerText = "Camera On";
+        cameraOff = true;
+    }
+}
+
+async function handleCamSelect() {
+    await getMedia(camSelect.value);
+}
+
+function handleWelcomeSubmit(event) {
     event.preventDefault();
-    const input = messageForm.querySelector("input");
-    socket.send(makeMessage("new_message", input.value));
-    // const li = document.createElement("li");
-    // li.innerText = `You : ${input.value}`; // 내가 쓴 메시지는 서버에서 오는게 아니라 내 프론트에서 그림
-    // messageList.append(li);
+    const input = welcomeForm.querySelector("input");
+    socket.emit("join_room", input.value);
     input.value = "";
-});
+} 
 
-nickForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname", input.value));
-    input.value = "";
-});
+// getMedia();
+
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
+camSelect.addEventListener("input", handleCamSelect); // camerasChange
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
--- src/server.js
+++ src/server.js
@@ -12,50 +12,14 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home")); // rendering
 app.get("/*", (_, res) => res.redirect("/"));
 
+const httpServer = http.createServer(app); // create server하려면 request Listner 경로가 있어야 함. 당연.
+const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", socket => {
+    socket.on("join_room", (roomName => {
+        socket.join(roomName);
+    }))
+})
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-// app.listen(3000, handleListen); // 시작하는 방법을 변경, ws사용
-
-// http 위에 wss를 얹어서 같은 서버 안에서 웹소켓까지 처리할 수 있게 됨
-// app.Listen과 다른 점?
-// 변화의 요점 : http에 access
-const server = http.createServer(app); // create server하려면 request Listner 경로가 있어야 함. 당연.
-
-/*
-const wss = new WebSocket.Server({server}); // http와 websocket 모두 작동시키기 (같은 포트에서)
-
-// fake db
-const sockets = [];
-
-function bindMsg(type, nickname, payload) {
-    const msg = {type, nickname, payload};
-    return JSON.stringify(msg);
-}
-
-// connection event. connection이 이루어지면 cb, function을 실행. socket이 필요
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anon";
-    console.log("Connected to Browser 😁");
-    socket.on("close", () => console.log("Disconnected from Browser.")); // 브라우저를 끄면 발생
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-
-        switch (message.type) {
-            case "new_message" :
-                sockets.forEach((aSocket) => {
-                    aSocket.send(bindMsg(message.type, aSocket.nickname, message.payload));
-                });
-                // sockets.forEach((aSocket) => aSocket.send(msg));
-                break;
-            case "nickname" :
-                socket["nickname"] = message.payload;
-                break;
-        }
-    });
-});
-*/
-
-// const wsServer = SocketIO(server);
-
-
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
